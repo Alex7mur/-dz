9a690ecb5f8ae083a4540200f1267f9c02fbcf4a
Alex7mur <marat046m@gmail.com>
2022-04-21 19:45:33 +0300

    Старт

---
+++ Alex++_JS_HW_1.1.2.js
@@ -0,0 +1,18 @@
+function verification(age) {
+let age_1 = age
+let age_2 = 18
+let age_3 = 60
+if (age_1 > 0) {
+    if (age_1 < age_2) {
+        return("You don\’t have access cause your age is " + age_1 + " It\’s less then")
+    } else if (age_1 >= age_2 && age_1 < age_3) {
+        return("Welcome !")
+    } else if (age_1 > age_3) {
+        return("Keep calm and look Culture channel")
+    }
+} else return("Technical work")
+}
+console.log(verification(17))
+console.log(verification(18))
+console.log(verification(61))
+console.log(verification(-5))
